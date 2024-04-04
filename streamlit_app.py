73f4d81c0a9be4316afd5f389012bc533342cb5d
LiyuGT <liyutenaw@gmail.com>
2024-04-04 00:15:41 -0400

    Clean up UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,14 +13,14 @@ st.dataframe(df)
 df["Order_Date"] = pd.to_datetime(df["Order_Date"])
 df.set_index('Order_Date', inplace=True)
 
-st.write("## Your additions")
-st.write("### (1) add a drop down for Category (https://docs.streamlit.io/library/api-reference/widgets/st.selectbox)")
+st.write("## Additions")
+st.write("### (1) Drop down for Category")
 category = st.selectbox("Select Category", df['Category'].unique())
 
-st.write("### (2) add a multi-select for Sub_Category *in the selected Category (1)* (https://docs.streamlit.io/library/api-reference/widgets/st.multiselect)")
+st.write("### (2) Multi-select for Sub_Category *in the selected Category*")
 sub_categories = st.multiselect("Select Sub-Category", df[df['Category'] == category]['Sub_Category'].unique())
 
-st.write("### (3) show a line chart of sales for the selected items in (2)")
+st.write("### (3) Line chart of sales for the selected items")
 def filter_data(category, sub_categories):
     filtered_df = df[(df['Category'] == category) & (df['Sub_Category'].isin(sub_categories))]
     return filtered_df
@@ -30,11 +30,11 @@ st.dataframe(filtered_df)
 
 # Here the Grouper is using our newly set index to group by Month ('M')
 sales_by_month2 = filtered_df.filter(items=['Sales']).groupby(pd.Grouper(freq='M')).sum()
-st.dataframe(sales_by_month2)
+# st.dataframe(sales_by_month2)
 # Here the grouped months are the index and automatically used for the x axis
 st.line_chart(sales_by_month2, y="Sales")
 
-st.write("### (4) show three metrics (https://docs.streamlit.io/library/api-reference/data/st.metric) for the selected items in (2): total sales, total profit, and overall profit margin (%)")
+st.write("### (4) Metrics: total sales, total profit, and overall profit margin")
 if not filtered_df.empty:
     total_sales = filtered_df['Sales'].sum()
     total_profit = filtered_df['Profit'].sum()
@@ -46,7 +46,7 @@ if not filtered_df.empty:
 
 
 
-st.write("### (5) use the delta option in the overall profit margin metric to show the difference between the overall average profit margin (all products across all categories)")
+st.write("### (5) Overall profit margin metric to show the difference between the overall average profit margin (all products across all categories)")
 if not filtered_df.empty:
     overall_avg_profit_margin = (df['Profit'].sum() / df['Sales'].sum()) * 100
     delta_margin = overall_profit_margin - overall_avg_profit_margin
